b39daaa1e061f1f6ba26d2975d47b6dc15ab7a74
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-28 07:05:49 +0300

    Fix .gitignore

--- Mourse_catalog/manage.py
+++ Mourse_catalog/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mource_catalog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mourse_catalog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
