e63fcdc478a3108c8cee7dd0345a0f0e1e4e147f
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-04-28 12:06:47 +0300

    Update urls.py
    App  MourseDetais path

--- Mourse_catalog/my_mourse/urls.py
+++ Mourse_catalog/my_mourse/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import MourseListView
+from .views import MourseDetaisView, MourseListView
 
 urlpatterns = [
     path('', MourseListView.as_view(), name='home'),
+    path('mourse/<int:pk>', MourseDetaisView.as_view(), name='mourse_detail'),
 ]
