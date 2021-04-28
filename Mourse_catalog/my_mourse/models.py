bd544f1e02ed50cf491386940f58271d398e8a9a
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-28 18:30:47 +0300

    Fix Pagination

--- Mourse_catalog/my_mourse/models.py
+++ Mourse_catalog/my_mourse/models.py
@@ -15,11 +15,11 @@ from django.urls import reverse
 
 class Mourse(models.Model):
     title = models.CharField(max_length=25)
-    # slug = models.SlugField(max_length = 250, null = True, blank = True)
+    # slug = models.SlugField(max_length = 250, null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     descr = models.TextField(max_length=255)
     start_date = models.DateField(default=date.today)
-    end_date = models.DateField()
+    end_date = models.DateField(default="2021-12-25", null=True, blank=True)
     q_lections = models.IntegerField(default=0)
 #     status = models.CharField(max_length = 10, choices = STATUS_CHOICES, default ='draft')
 
