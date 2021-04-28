108845444614ed759ec30f7a4a27819cbf922bf7
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-04-28 14:14:26 +0300

    Update views.py
    Add slug filter @ views

--- Mourse_catalog/my_mourse/views.py
+++ Mourse_catalog/my_mourse/views.py
@@ -16,8 +16,29 @@ class MourseListView(ListView):
 #     model = Mourse
 #     template_name = 'home.html'
    
-class MourseDetaisView(DetailView):
+class MourseDetaisView(DetailView, slug):
     model = Mourse
     template_name = 'mourse_details.html'
+    q = Mourse.objects.filter(slug__iexact = slug)
+    if q.exists():
+        q = q.first()
+    else:
+        return HttpResponse('<h1>Mourse is Not Found</h1>')
+    context = {
+        'mourse': q
+    }
+    return render(request, 'mourse_details.html', context)
    
   
+# TOFIX 
+# def detail(request, slug):
+#     q = Post.objects.filter(slug__iexact = slug)
+#    if q.exists():
+#        q = q.first()
+#    else:
+#        return HttpResponse('<h1>Post Not Found</h1>')
+#    context = {
+ 
+#        'post': q
+#    }
+#    return render(request, 'posts/details.html', context)
