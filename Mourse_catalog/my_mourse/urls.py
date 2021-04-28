ae7ccb63dcc0619e7ba7a09805870642ee63528c
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-04-28 15:32:51 +0300

    Update urls.py
    Connect MourseCreateView

--- Mourse_catalog/my_mourse/urls.py
+++ Mourse_catalog/my_mourse/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MourseDetaisView, MourseListView
+from .views import MourseCreateView, MourseDetaisView, MourseListView
 
 urlpatterns = [
     path('', MourseListView.as_view(), name='home'),
     path('mourse/<int:pk>', MourseDetaisView.as_view(), name='mourse_detail'),
+    path('create_post/'), MourseCreateView.as_view(), name='create_mourse'),
 ]
