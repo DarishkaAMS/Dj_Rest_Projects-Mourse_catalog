d0f26d56f023ef88f95b35ca50e76e88a6a92dc1
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-04-28 11:36:03 +0300

    Update views.py
    Add class HomeView

--- Mourse_catalog/my_mourse/views.py
+++ Mourse_catalog/my_mourse/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Mourse
+ # Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html', {})
+class HomeView(ListView):
+    model = Mourse
+    template_name = 'home.html'
