c2431ccf7cb313082b6f9c9f42700ea977bbde55
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-29 06:59:58 +0300

    Style the form

--- Mourse_catalog/my_mourse/forms.py
+++ Mourse_catalog/my_mourse/forms.py
@@ -8,9 +8,9 @@ class MourseForms(forms.ModelForm):
         model = Mourse
         fields = ('title', 'author', 'description', 'q_lectures')
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'How should we call it?'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'What is it about?'}),
             'start_date': forms.DateInput(attrs={'class': 'form-control'}),
             'end_date': forms.DateInput(attrs={'class': 'form-control'}),
             'q_lectures': forms.NumberInput(attrs={'class': 'form-control'}),
