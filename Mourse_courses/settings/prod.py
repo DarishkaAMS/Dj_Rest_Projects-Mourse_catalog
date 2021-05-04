01d04ca44a5df85878be36b406de1b92dd0967ae
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-04 05:52:27 +0300

    Update settings location and split

---
+++ Mourse_courses/settings/prod.py
@@ -0,0 +1,7 @@
+import os
+from .base import *
+
+SECRET_KEY = 'q1ui$9l5xtc*q^y!p8bh3egdhxtxn@vm&652mg^yctu4=$+w%t'
+# SECRET_KEY = os.environ("DJANGO_SECRET_KEY")
+DEBUG = False
+ALLOWED_HOSTS = ["localhost"]
